fb89b4568fa3bbb3cddafe56d135c925d8de33ac
James Houck <jameshouck@live.com>
2019-08-29 15:50:53 -0500

    initial commit

---
+++ part1.py
@@ -0,0 +1,12 @@
+#Part 1: Hello, birthday month
+
+#Write a program that asks for your name and the month you were born in.
+name = input('What is your name?\n')
+month = input('What month were you born?\n')
+
+#Then, your program should print
+
+#A greeting to you, using your name
+print("Hello, ", name, ". Welcome to the Matrix.")
+#A message with the number of letters in your name
+#A 'Happy birthday month!' message if you were born in August
